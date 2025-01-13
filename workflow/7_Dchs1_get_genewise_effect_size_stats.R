15c5be7743d736f03d2787da1feb97076a0033de
ghalasz <1130238+ghalasz@users.noreply.github.com>
2025-01-13 14:51:26 -0500

    second upload, w/ directory structure and additional files

---
+++ workflow/7_Dchs1_get_genewise_effect_size_stats.R
@@ -0,0 +1,86 @@
+library(dplyr)
+
+# INIT
+NUM_TRIALS <- 40
+N_RANGE <- 3:28
+PTYPE <- "Padj"
+PADJ_CUTOFF <- 0.05
+TISSUES_IN_ORDER <- c('heart', 'kidney', 'liver', 'lung')
+# USE_ORIG_GENES <- FALSE # set in wrapper?
+
+
+# Randomly select 20 genes per tissue, grouped into four fold change ranges
+  gold_standards <- readRDS("result_objs/gold_standards.RDS")
+  genes.for.effect.size.exploration <- list()
+  num.per.group <- 5  # 5 genes for each combo of tissue-by-fold.change.regime
+  set.seed(17)
+  for (this.tissue in TISSUES_IN_ORDER) {
+    set2 <-   setdiff(unlist(gold_standards$Dchs1[[paste0(this.tissue, ",0,Padj,0.05,2")]]), "")  # genes with FC >= 2 or FC < 0.5
+    set1.5 <- setdiff(unlist(gold_standards$Dchs1[[paste0(this.tissue, ",0,Padj,0.05,1.5")]]), set2) # genes with (1.5 < FC < 2) OR (1/2 < FC < 2/3)
+    set1.2 <- setdiff(unlist(gold_standards$Dchs1[[paste0(this.tissue, ",0,Padj,0.05,1.2")]]), c(set2, set1.5)) # genes with (1.2 < FC < 1.5) OR (2/3 < FC < 5/6)
+    set1 <-   setdiff(unlist(gold_standards$Dchs1[[paste0(this.tissue, ",0,Padj,0.05,1")]]), c(set2, set1.5, set1.2)) # genes with (5/6 < FC < 1.2)
+    genes.for.effect.size.exploration[[this.tissue]] <- c(
+      sample(set1, num.per.group, replace = FALSE),
+      sample(set1.2, num.per.group, replace = FALSE),
+      sample(set1.5, num.per.group, replace = FALSE),
+      sample(set2, num.per.group, replace = FALSE))
+  }
+  saveRDS(genes.for.effect.size.exploration, 'result_objs/Dchs1_genes.for.effect.size.exploration.RDS')
+
+
+uniq.explore.genes <- as.character(unlist(genes.for.effect.size.exploration))
+genewise.effect.size.res <- data.frame(expand.grid(gene = uniq.explore.genes,
+                                                   tissue = TISSUES_IN_ORDER,
+                                                   tpm.cutoff = c(0, 1, 5),
+                                                   N = N_RANGE, trial = 1:NUM_TRIALS,
+                                                   FC = NA, is.signif = NA, meanX.TPM = NA,
+                                                   stringsAsFactors = FALSE))
+
+prev.DEres.file <- ""
+block_size <- length(uniq.explore.genes) * length(TISSUES_IN_ORDER)
+for (i in seq(1, nrow(genewise.effect.size.res), by = block_size)) { 
+  tpm.cutoff <- genewise.effect.size.res[i, 'tpm.cutoff']
+  this.N <- genewise.effect.size.res[i,"N"]
+  this.trial <- genewise.effect.size.res[i,"trial"]
+  this_range <- i:(i+block_size-1)
+  stopifnot(all(genewise.effect.size.res[this_range, 'tpm.cutoff'] == tpm.cutoff))
+  stopifnot(all(genewise.effect.size.res[this_range, 'trial'] == this.trial))
+  stopifnot(all(genewise.effect.size.res[this_range, 'N'] == this.N))
+  if (i %% 10000 < 50) { message(paste(i, date())) }
+  
+  # read in DE results
+  this.DEres.file <- paste0("DE_runs_subsample//Dchs1_N", genewise.effect.size.res[i,"N"],
+                            "_trial", genewise.effect.size.res[i, "trial"], ".RDS")
+  if (this.DEres.file != prev.DEres.file) {
+    this.DEres <- readRDS(this.DEres.file)
+  }
+  prev.DEres.file <- this.DEres.file
+  
+  for (j in seq(i, max(this_range), by = length(uniq.explore.genes))) {
+    tissue <- genewise.effect.size.res[j, "tissue"]
+    this_tissue_range <- j:(j+length(uniq.explore.genes)-1)
+    stopifnot(genewise.effect.size.res[this_tissue_range, 'gene'] == uniq.explore.genes)
+    stopifnot(all(genewise.effect.size.res[this_tissue_range, 'tissue'] == tissue))
+    
+    #shorthand
+    this.pvals <- this.DEres[[PTYPE]][uniq.explore.genes, tissue]
+    this.fcs <- this.DEres$Fold.Change[uniq.explore.genes, tissue]
+    this.tpm <- this.DEres$meanY.TPM[uniq.explore.genes, tissue]
+    alt.tpm <- this.DEres$meanX.TPM[uniq.explore.genes, tissue]
+    downreg <- !is.na(this.fcs) & this.fcs < 1
+    this.tpm[downreg] <- alt.tpm[downreg]
+    
+    # figure out if significant
+    tmp.pass <- !is.na(this.fcs) & !is.na(this.pvals)
+    tmp.pass <- tmp.pass & this.pvals < PADJ_CUTOFF
+    tmp.pass <- tmp.pass & this.tpm >= tpm.cutoff
+    
+    genewise.effect.size.res[this_tissue_range, 'FC'] <- this.fcs
+    genewise.effect.size.res[this_tissue_range, 'is.signif'] <- tmp.pass
+    genewise.effect.size.res[this_tissue_range, 'meanX.TPM'] <- this.tpm
+  }  
+}
+head(genewise.effect.size.res)
+table(genewise.effect.size.res$is.signif, useNA = 'always')
+
+saveRDS(genewise.effect.size.res, 'result_objs/Dchs1_genewise.effect.size.res.RDS')
